f8ffb3a2763d6c1e810486dbfa19aadf088a97e0
Akbar Yudhanto <50676898+Dansday@users.noreply.github.com>
2023-03-29 00:52:27 +0800

    Add files via upload

---
+++ PIR/pir/pir.ino
@@ -0,0 +1,31 @@
+//Inisialisasi pin
+int ledPin = 13;
+int inputPin = A0;
+int pirState = LOW;
+int val = 0;
+void setup()
+{
+pinMode(ledPin, OUTPUT);
+pinMode(inputPin, INPUT);
+Serial.begin(9600);
+}
+void loop()
+{
+val = digitalRead(inputPin);
+if (val == HIGH)
+{
+digitalWrite(ledPin, HIGH);
+if (pirState == HIGH) {
+Serial.println("Gerakan Terdeteksi!");
+pirState = LOW;
+}
+}
+else
+{
+digitalWrite(ledPin, LOW);
+if (pirState == LOW){
+Serial.println("Tidak Ada Gerakan!");
+pirState = HIGH;
+}
+}
+}
